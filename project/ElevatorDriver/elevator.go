200a837f36b916e4bea25371b2a7930cfd70b552
Joemgu <jegunnarshaug@gmail.com>
2026-02-04 09:29:02 +0100

    added the printing elevator

--- project/ElevatorDriver/elevator.go
+++ project/ElevatorDriver/elevator.go
@@ -1,6 +1,8 @@
 
 package ElevatorDriver
 
+import "fmt"
+
 // This is just for development purposes
 // In the real system, these constants and types would be provided by the config file
 const N_FLOORS = 4
@@ -30,4 +32,33 @@ type Elevator struct {
 }
 
 
+func StateToString(b ElevatorBehaviour) string {
+    switch b {
+    case Idle:     return "Idle"
+    case Moving:   return "Moving"
+    case DoorOpen: return "DoorOpen"
+    default:       return "Unknown"
+    }
+}
 
+func ElevatorPrint(e Elevator) {
+    fmt.Printf("  +--------------------+\n")
+    fmt.Printf("  |floor = %-2d          |\n", e.lastFloor)
+    fmt.Printf("  |Direction  = %-12s|\n", DirnToString(e.Direction))
+    fmt.Printf("  |Behaviour = %-12s|\n", StateToString(e.Behaviour))
+    fmt.Printf("  +--------------------+\n")
+    fmt.Printf("  |  Up  | Down |  Cab |\n")
+
+    for f := NumFloors - 1; f >= 0; f-- {
+        fmt.Printf("  |")
+        for b := 0; b < NumButtons; b++ {
+            if e.Requests[f][b] {
+                fmt.Printf("  #   ")
+            } else {
+                fmt.Printf("  -   ")
+            }
+        }
+        fmt.Printf("| %d\n", f)
+    }
+    fmt.Printf("  +--------------------+\n")
+}
